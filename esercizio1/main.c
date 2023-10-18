ddd1312769cf37c3f69ee23b50240d59a1d8393d
Francesco Orlando <91286116+Effeo@users.noreply.github.com>
2023-10-18 11:16:49 +0200

    des

--- esercizio1/main.c
+++ esercizio1/main.c
@@ -3,11 +3,166 @@
 
 #include "mpi.h"
 
-/*
-int stretegyOne(int argc, char *argv[]);
-int stretegyTwo(int argc, char *argv[]);
-int strategyThree(int argc, char *argv[]);
-*/
+int stretegyTwo(int argc, char *argv[])
+{
+  int startIndex = 2;
+  int nOfNumbers = atoi(argv[startIndex++]);
+
+  int menum, nProcessors;
+
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &menum);
+  MPI_Comm_size(MPI_COMM_WORLD, &nProcessors);
+
+  // If it's not a power of two, return -1
+  if (nProcessors & (nProcessors - 1) != 0)
+  {
+    MPI_Finalize();
+    return stretegyOne(argc, argv);
+  }
+
+  int fractions = nOfNumbers / nProcessors;
+
+  int min = menum * fractions;
+  int max = min + fractions;
+
+  int sum = 0;
+  int value = 0;
+  int currentLevel = 1;
+  int i;
+
+  if(menum == 0)
+  {
+      max =  max + nOfNumbers % nProcessors;
+  }
+  
+  for (i = min; i < max; ++i)
+  {
+    sum += atoi(argv[startIndex + i]);
+  }
+
+  MPI_Status status;
+
+  int levels = (int)(log(nProcessors) / log(2));
+
+  for (currentLevel = 1; currentLevel <= levels; ++currentLevel)
+  {
+    if (menum % (int)pow(2, currentLevel) == 0)
+    {
+      // Receiver
+      int from = menum + (int)(pow(2, currentLevel) * 0.5);
+
+      MPI_Recv(&value, 1, MPI_INT, from, 0, MPI_COMM_WORLD, &status);
+      sum += value;
+    }
+    else if (menum % (int)pow(2, currentLevel) == (int)(pow(2, currentLevel) * 0.5))
+    {
+      //proviamo con break o exit
+      // Sender
+
+      int to = menum - (menum % ((int)pow(2, currentLevel)));
+
+      MPI_Send(&sum, 1, MPI_INT, to, 0, MPI_COMM_WORLD);
+    }
+  }
+
+  MPI_Finalize();
+
+  if(menum == 0){
+    printf("Result: %d\n", sum);
+    return sum;
+  }
+
+  return 0;
+}
+
+int strategyThree(int argc, char *argv[])
+{
+  int startIndex = 2;
+  int nOfNumbers = atoi(argv[startIndex++]);
+
+  int menum, nProcessors;
+
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &menum);
+  MPI_Comm_size(MPI_COMM_WORLD, &nProcessors);
+
+  // If it's not a power of two, return -1
+  if (nProcessors & (nProcessors - 1) != 0)
+  {
+    MPI_Finalize();
+    return stretegyOne(argc, argv);
+  }
+
+  int fractions = nOfNumbers / nProcessors;
+
+  int min = menum * fractions;
+  int max = min + fractions;
+
+  int sum = 0;
+  int value = 0;
+  int currentLevel = 1;
+  int i;
+
+  if(menum == 0)
+  {
+      max =  max + nOfNumbers % nProcessors;
+  }
+  
+  for (i = min; i < max; ++i)
+  {
+    sum += atoi(argv[startIndex + i]);
+  }
+
+  MPI_Status status;
+  int levels = nProcessors / 2;
+  int scarto;
+  int verso;
+  int salto;
+ 
+  for(currentLevel = 1; currentLevel < levels; ++currentLevel)
+  {
+    scarto = (int)pow(2, currentLevel - 1);
+    verso = (menum / scarto);
+
+    if(verso % 2 == 0)
+    {
+      salto = menum + scarto;
+    }
+    else
+    {
+      salto = menum - scarto;
+    }
+
+    if(menum % 2 == 0){
+      //Send recive
+
+      printf("Send recive: proc %d sending to %d\n", menum, salto);
+      MPI_Send(&sum, 1, MPI_INT, salto, 0, MPI_COMM_WORLD);
+      printf("Inviato\n")
+      MPI_Recv(&value, 1, MPI_INT, salto, 0, MPI_COMM_WORLD, &status);
+
+      sum += value;
+    }
+    else 
+    {
+      //recive send
+      printf("Recive send: proc %d sending to %d\n", menum, salto);
+      MPI_Recv(&value, 1, MPI_INT, salto, 0, MPI_COMM_WORLD, &status);
+      printf("Ricevuto\n")
+      MPI_Send(&sum, 1, MPI_INT, salto, 0, MPI_COMM_WORLD);
+
+      sum += value;
+    }
+  }
+
+
+  MPI_Finalize();
+  printf("Result: %d\n", sum);
+  return sum;
+}
 
 int stretegyOne(int argc, char *argv[])
 {
@@ -24,11 +179,12 @@ int stretegyOne(int argc, char *argv[])
   MPI_Comm_rank(MPI_COMM_WORLD, &menum);
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
-  int fractions = nOfNumbers / nproc; // 100 / 8
+  int fractions = nOfNumbers / nproc;
 
   int min = menum * fractions;
   int max = min + fractions;
   int i = 0;
+  
   // Controller
   if (menum == 0)
   {
@@ -37,151 +193,62 @@ int stretegyOne(int argc, char *argv[])
     int sum = 0;
     int mmax = max + nOfNumbers % nproc;
 
-    printf("Executing sum from 0 to %d\n", mmax);
-
     for (i = min; i < mmax; ++i)
     {
       sum += atoi(argv[startIndex + i]);
     }
 
-    printf("Waiting for process to send data:\n");
     for (i = 1; i < nproc; ++i)
     {
       int value = 0;
 
       MPI_Recv(&value, 1, MPI_INT, i, i, MPI_COMM_WORLD, &status);
-      printf("Status from rank %d, with tag %d and error code %d.\n", status.MPI_SOURCE, status.MPI_TAG, status.MPI_ERROR);
       sum += value;
     }
 
     printf("Result: %d\n", sum);
+    MPI_Finalize();
+    return sum;
   }
   else // Calculators
   {
-    printf("Executing sum from %d to %d\n", min, max);
-
     int sum = 0;
+
     for (i = min; i < max; ++i)
     {
       sum += atoi(argv[startIndex + i]);
     }
 
-    printf("Sending sum %d to process 0\n", sum);
     MPI_Send(&sum, 1, MPI_INT, 0, menum, MPI_COMM_WORLD);
   }
 
   MPI_Finalize();
-  return 0;
-}
 
-int stretegyTwo(int argc, char *argv[])
-{
-  // Just read comment on strategyOne
-  int startIndex = 2;
-
-  int nOfNumbers = atoi(argv[startIndex++]);
-
-  int rank, nProcessors;
-  MPI_Init(&argc, &argv);
-
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &nProcessors);
-
-  // If it's not a power of two, return -1
-  if (nProcessors & (nProcessors - 1) != 0)
-  {
-    MPI_Finalize();
-    return -1;
-  }
-
-  int fractions = nOfNumbers / nProcessors; // 100 / 8
-
-  int min = rank * fractions;
-  int max = min + fractions;
-
-  int sum = 0;
-  int value = 0;
-  int currentLevel = 1;
-
-  for (i = min; i < max; ++i)
-  {
-    sum += atoi(argv[startIndex + i]);
-  }
-  printf("[Rank %d]: Calculated SUM from %d to %d. Total is %d\n", rank, min, max, sum);
-
-  MPI_Status status;
-
-  /*
-  if (rank % (int)pow(2, currentLevel) == 0)
-  {
-    int from = rank + (int)(pow(2, currentLevel) * 0.5);
-    MPI_Recv(&value, 1, MPI_INT, from, 0, MPI_COMM_WORLD, &status);
-
-    sum += value;
-  }
-  else
-  {
-    int to = rank - (rank % ((int)pow(2, currentLevel)));
-    MPI_Send(&sum, 1, MPI_INT, to, 0, MPI_COMM_WORLD);
-  }
-  */
-
-  int levels = (int)(log(nProcessors) / log(2));
-  int currentProcess;
-  for (currentLevel = 1; currentLevel <= levels; ++currentLevel)
-  {
-    if (rank % (int)pow(2, currentLevel) == 0)
-    {
-      // Receiver
-      int from = rank + (int)(pow(2, currentLevel) * 0.5);
-      MPI_Recv(&value, 1, MPI_INT, from, 0, MPI_COMM_WORLD, &status);
-      sum += value;
-
-      printf("[Rank %d]: Received from %d sum %d. Total sum: %d\n", rank, from, value, sum);
-    }
-    else if (rank % (int)pow(2, currentLevel) == (int)(pow(2, currentLevel) * 0.5))
-    {
-      // Sender
-
-      int to = rank - (rank % ((int)pow(2, currentLevel)));
-      printf("[Rank %d]: Sending sum %d to process %d\n", rank, sum, to);
-
-      MPI_Send(&sum, 1, MPI_INT, to, 0, MPI_COMM_WORLD);
-    }
-  }
-
-  if (rank == 0)
-  {
-    printf("Sum: %d\n", sum);
-  }
-
-  MPI_Finalize();
-  return 0;
-}
-
-int strategyThree(int argc, char *argv[])
-{
   return 0;
 }
 
 int main(int argc, char *argv[])
 {
   int strategyNumber = atoi(argv[1]);
+  int result;
 
-  int shouldRunOne = strategyNumber == 1;
-
-  if (strategyNumber == 2)
+  if (strategyNumber == 1)
   {
-    shouldRunOne = stretegyTwo(argc, argv) == -1;
+    result = stretegyOne(argc, argv);
+  }
+  else if (strategyNumber == 2)
+  {
+    result = stretegyTwo(argc, argv);
   }
   else if (strategyNumber == 3)
   {
-    shouldRunOne = strategyThree(argc, argv) == -1;
+    result = strategyThree(argc, argv);
+  }
+  else
+  {
+    printf("Selezionare una strategia corretta\n");
+    return -1;
   }
 
-  if (shouldRunOne == 1)
-  {
-    return stretegyOne(argc, argv);
-  }
   return 0;
 }
