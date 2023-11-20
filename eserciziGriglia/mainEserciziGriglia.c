7a99d1b01cdb6dd96d6c81ea3723c318b76de067
Francesco Orlando <91286116+Effeo@users.noreply.github.com>
2023-11-20 17:33:30 +0100

    Update mainEserciziGriglia.c

--- eserciziGriglia/mainEserciziGriglia.c
+++ eserciziGriglia/mainEserciziGriglia.c
@@ -125,8 +125,98 @@ void esercizio2(int argc, char* argv[]){
     }
 }
 
+void esercizio3(int argc, char* argv[]){
+    int menum, nproc, row, col, menum_grid;
+    int dim, *ndim, reorder, *period, *coordinate;
+    int i, j;
+    MPI_Comm comm_grid;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &menum);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+
+    // numero di righe della griglia di processo
+    row = atoi(argv[1]);
+    int n = atoi(argv[2]);
+    int m = atoi(argv[3]);
+
+    int** mat = (int**) malloc(n * sizeof(int*));
+
+    for (i = 0; i < n; i++){
+        mat[i] = (int*) malloc(m * sizeof(int));
+    }
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            mat[i][j] = atoi(argv[4 + i * m + j]);
+        }
+    }
+
+    
+    // spedizione id row da parte di 0 a tutti i processi
+    MPI_Bcast(&row, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // definizione del numero di colonne della griglia
+    col = nproc / row;
+
+    // numero di dimensioni della griglia
+    dim = 2;
+    coordinate = (int*) calloc(dim, sizeof(int));
+
+    // vettore contenente le lunghezze di ciascuna dimesione
+    ndim = (int*) calloc(dim, sizeof(int));
+    ndim[0] = row;
+    ndim[1] = col;
+
+    // vettore contenente le periodicità delle dimensioni
+    period = (int*) calloc(dim, sizeof(int));
+    period[0] = period[1] = 0;
+    reorder = 0;
+
+    // definizione della griglia bidimensionale
+    MPI_Cart_create(MPI_COMM_WORLD, dim, ndim, period, reorder, &comm_grid);
+    // nella slide c'è scritto &menum_grid, ma non è definito
+    MPI_Comm_rank(comm_grid, &menum);
+
+    // definizione delle coordinate di ciascun processo nella griglia bidimensionale
+    MPI_Cart_coords(comm_grid, menum, dim, coordinate);
+    
+    if (n % row == 0 && m % col == 0){
+        int num_elements = (m * n) / (row * col);
+        
+        int** proc_elements = (int**) malloc(nproc * sizeof(int*));
+        for (i = 0; i < nproc; i++){
+            proc_elements[i] = (int*) malloc(num_elements * sizeof(int));
+        }
+
+        int row_jump = n / row;
+        int col_jump = m / col; 
+        int k;
+        
+        for (k = 0; k < nproc; k++){
+            int proc_row = k / col;
+            int proc_col = k % col;
+            int proc_index = 0;
+            for (i = proc_row * row_jump; i < (proc_row + 1) * row_jump; i++){
+                for (j = proc_col * col_jump; j < (proc_col + 1) * col_jump; j++){
+                    proc_elements[k][proc_index] = mat[i][j];
+                    proc_index++;
+                }
+            }
+        }
+
+        
+    }
+    else{
+        printf("Impossibile dividere la matrice in sottomatrici\n");
+    }
+
+    MPI_Finalize();
+}
+
 int main(int argc, char* argv[]) {
     //esercizio1(argc, argv);
-    esercizio2(argc, argv);
+    //esercizio2(argc, argv);
+    esercizio3(argc, argv);
     return 0;
 }
